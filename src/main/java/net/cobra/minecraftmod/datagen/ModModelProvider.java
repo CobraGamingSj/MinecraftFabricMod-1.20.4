d45d840d577e8e7e9efc9fae8c42c992e5b03a45
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2024-04-13 20:33:02 +0530

    New Food & Fuel

---
+++ src/main/java/net/cobra/minecraftmod/datagen/ModModelProvider.java
@@ -0,0 +1,39 @@
+package net.cobra.minecraftmod.datagen;
+
+import net.cobra.minecraftmod.block.ModBlocks;
+import net.cobra.minecraftmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_BLOCK);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RUBY_ORE);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_RUBY_ORE);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RADIANT_BLOCK);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SAPPHIRE_BLOCK);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SAPPHIRE_ORE);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_SAPPHIRE_ORE);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.GREEN_SAPPHIRE_BLOCK);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.GREEN_SAPPHIRE_ORE);
+       blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_GREEN_SAPPHIRE_ORE);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+      itemModelGenerator.register(ModItems.RUBY, Models.GENERATED);
+      itemModelGenerator.register(ModItems.RADIANT, Models.GENERATED);
+      itemModelGenerator.register(ModItems.SAPPHIRE, Models.GENERATED);
+      itemModelGenerator.register(ModItems.GREEN_SAPPHIRE, Models.GENERATED);
+      itemModelGenerator.register(ModItems.PINEAPPLE, Models.GENERATED);
+      itemModelGenerator.register(ModItems.WOOD_PELLET, Models.GENERATED);
+    }
+}

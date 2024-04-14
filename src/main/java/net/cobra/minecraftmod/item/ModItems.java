54872eb561b1613a6beadbf4e1e471529e2feb88
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2024-04-14 16:49:45 +0530

    Armors & Tools

--- src/main/java/net/cobra/minecraftmod/item/ModItems.java
+++ src/main/java/net/cobra/minecraftmod/item/ModItems.java
@@ -24,6 +24,11 @@ public class ModItems {
     public static final Item RUBY_SHOVEL = registerItem("ruby_shovel", new ShovelItem(ModToolMaterial.RUBY, 2, 1.5f, new FabricItemSettings()));
     public static final Item RUBY_HOE = registerItem("ruby_hoe", new HoeItem(ModToolMaterial.RUBY, -3, 0, new FabricItemSettings()));
 
+    public static final Item RUBY_HELMET = registerItem("ruby_helmet", new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new FabricItemSettings()));
+    public static final Item RUBY_CHESTPLATE = registerItem("ruby_chestplate",  new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
+    public static final Item RUBY_LEGGINGS = registerItem("ruby_leggings", new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
+    public static final Item RUBY_BOOTS = registerItem("ruby_boots", new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.BOOTS, new FabricItemSettings()));
+
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
         entries.add(RADIANT);
@@ -43,6 +48,10 @@ public class ModItems {
    }
     private static void addToolsToCombatGroup(FabricItemGroupEntries entries) {
         entries.add(ModItems.RUBY_SWORD);
+        entries.add(ModItems.RUBY_HELMET);
+        entries.add(ModItems.RUBY_CHESTPLATE);
+        entries.add(ModItems.RUBY_LEGGINGS);
+        entries.add(ModItems.RUBY_BOOTS);
     }
 
 
